dc4d3f8656aaf583c56a1e4783c8b02a4e1a9956
Steve Cochrane <steve@stevecochrane.com>
2021-11-03 22:13:27 -0500

    Replace createStore with configureStore

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { Provider } from "react-redux";
 import { reducer } from "./mood";
 import App from "./App";
@@ -8,7 +8,7 @@ import App from "./App";
 require("./styles.css");
 
 //store
-const store = createStore(reducer);
+const store = configureStore({ reducer });
 
 ReactDOM.render(
   <React.StrictMode>
