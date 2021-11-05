1eaa3acf859a9087b8f6adbc1bcb3348e6defbc3
Steve Cochrane <steve@stevecochrane.com>
2021-11-05 13:35:43 -0500

    Replace plain reducer with createReducer

--- src/mood.js
+++ src/mood.js
@@ -1,4 +1,4 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const MOODS = {
   SAD: "sad",
@@ -9,18 +9,19 @@ export const MOODS = {
   EXCITED: "excited",
   KO: "ko"
 };
+
 const INITIAL_STATE = { mood: MOODS.SAD };
 
 const UPDATE_MOOD = "UPDATE_MOOD";
 export const updateCatMood = createAction(UPDATE_MOOD);
 
-//reducer
-export const reducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case updateCatMood.type:
-      return { ...state, mood: action.payload };
-
-    default:
-      return state;
-  }
+export const reducer = createReducer(
+  INITIAL_STATE,
+  {
+    [updateCatMood]: (state, action) => {
+      state.mood = action.payload;
+    }
+  },
+  [],
+  (state) => state
 };
