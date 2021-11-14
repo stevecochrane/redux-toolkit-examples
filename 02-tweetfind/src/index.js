03271f721471f2a40a5614ef1a530eb334218c0d
Steve Cochrane <steve@stevecochrane.com>
2021-11-14 14:05:15 -0600

    Add ChakraProvider to index file

--- 02-tweetfind/src/index.js
+++ 02-tweetfind/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ChakraProvider } from '@chakra-ui/react';
 
 import './index.css';
 import store from './app/store';
@@ -11,7 +12,9 @@ const render = () => {
   ReactDOM.render(
     <React.StrictMode>
       <Provider store={store}>
-        <App />
+        <ChakraProvider>
+          <App />
+        </ChakraProvider>
       </Provider>
     </React.StrictMode>,
     document.getElementById("root")
