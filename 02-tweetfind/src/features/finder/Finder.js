8209cfdd515d1bb562aa9a6fcaf266edafd26c61
Steve Cochrane <steve@stevecochrane.com>
2021-11-14 14:16:38 -0600

    Add styled input and search submit button

--- 02-tweetfind/src/features/finder/Finder.js
+++ 02-tweetfind/src/features/finder/Finder.js
@@ -1,5 +1,19 @@
-import React from "react";
+import { Flex, Input, IconButton } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 export function Finder() {
-  return <div>Hi</div>;
+  return (
+    <Flex alignItems="center">
+      <Input
+        mr={3}
+        placeholder="enter a theme or hashtag to search"
+        size="lg"
+      />
+      <IconButton
+        aria-label="Search Twitter"
+        colorScheme="blue"
+        icon={<SearchIcon />}
+      />
+    </Flex>
+  );
 }
