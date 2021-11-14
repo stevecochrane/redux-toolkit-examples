8026e4f397b85049cb7dddf843064d5373c585d5
Steve Cochrane <steve@stevecochrane.com>
2021-11-14 14:23:01 -0600

    Add NumberOfResults and integrate into Finder

--- 02-tweetfind/src/features/finder/Finder.js
+++ 02-tweetfind/src/features/finder/Finder.js
@@ -1,19 +1,24 @@
 import { Flex, Input, IconButton } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
+import { NumberOfResults } from "../numberOfResults/NumberOfResults";
+
 export function Finder() {
   return (
-    <Flex alignItems="center">
-      <Input
-        mr={3}
-        placeholder="enter a theme or hashtag to search"
-        size="lg"
-      />
-      <IconButton
-        aria-label="Search Twitter"
-        colorScheme="blue"
-        icon={<SearchIcon />}
-      />
-    </Flex>
+    <>
+      <Flex alignItems="center">
+        <Input
+          mr={3}
+          placeholder="enter a theme or hashtag to search"
+          size="lg"
+        />
+        <IconButton
+          aria-label="Search Twitter"
+          colorScheme="blue"
+          icon={<SearchIcon />}
+        />
+      </Flex>
+      <NumberOfResults />
+    </>
   );
 }
