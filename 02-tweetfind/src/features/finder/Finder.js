f1037f620b701d54290ddca3211ad92461ea999a
Steve Cochrane <steve@stevecochrane.com>
2021-11-17 21:26:21 -0600

    Set up data fetching in Finder and store in local state

--- 02-tweetfind/src/features/finder/Finder.js
+++ 02-tweetfind/src/features/finder/Finder.js
@@ -1,21 +1,35 @@
+import React, { useState } from "react";
 import { Flex, Input, IconButton } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
 import { NumberOfResults } from "../numberOfResults/NumberOfResults";
+import { findTweets } from "./findTweets";
 
 export function Finder() {
+  const [searchValue, setSearchValue] = useState("");
+  const handleSearch = async () => {
+    if (searchValue) {
+      setSearchValue("");
+      const data = await findTweets(searchValue, 10);
+      console.log({ data });
+    }
+  };
+
   return (
     <>
       <Flex alignItems="center">
         <Input
           mr={3}
+          onChange={(evt) => setSearchValue(evt.target.value)}
           placeholder="enter a theme or hashtag to search"
           size="lg"
+          value={searchValue}
         />
         <IconButton
           aria-label="Search Twitter"
           colorScheme="blue"
           icon={<SearchIcon />}
+          onClick={handleSearch}
         />
       </Flex>
       <NumberOfResults />
