80ae12161e9c55becac144d92b004bc48c68cdf6
Steve Cochrane <steve@stevecochrane.com>
2021-11-22 15:10:02 -0600

    Add fetchTweets thunk to finderSlice

--- 02-tweetfind/src/features/finder/finderSlice.js
+++ 02-tweetfind/src/features/finder/finderSlice.js
@@ -1,18 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { findTweets } from "./findTweets";
 
-const initialState = { tweets: [] };
+export const fetchTweets = (searchValue, numberOfResults) => async (dispatch) => {
+    dispatch(isLoadingTweets());
+    const tweets = await findTweets(searchValue, numberOfResults);
+    dispatch(loadingTweetsSuccess(tweets));
+};
+
+const initialState = { tweets: [], isLoading: false, error: null };
 
 // create the slice
 const finderSlice = createSlice({
   name: "finder",
   initialState,
   reducers: {
-    addTweet(state, payload) {
+    loadingTweetsSuccess(state, {payload}) {
       state.tweets = payload;
+      state.isLoading = false;
+      state.error = null;
     },
-  },
+    isLoadingTweets(state) {
+      state.isLoading = true;
+    }
+  }
 });
 
 // export the action creator and reducer
-export const { addTweet } = finderSlice.actions;
+export const { isLoadingTweets, loadingTweetsSuccess } = finderSlice.actions;
 export default finderSlice.reducer;
